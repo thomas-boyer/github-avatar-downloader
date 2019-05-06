a1cbc6e082503d1f6e7f482a6009c1d9af590447
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-06 21:59:41 +0000

    Added skeleton for getRepoContributors() function

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,14 @@
 let request = require('request');
+let args = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+console.log(args);
+
+function getRepoContributors(repoOwner, repoName, callback) {
+  return callback;
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
