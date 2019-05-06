3ca49915a40a56d41ba5698afd96f938fbd0099d
Thomas Boyer <thomas.james.boyer@gmail.com>
2019-05-06 22:16:48 +0000

    First steps of getRepoContributors() function, added auth key in separate file to gitignore

--- download-avatars.js
+++ download-avatars.js
@@ -1,11 +1,26 @@
 let request = require('request');
+let secrets = require('./secrets.js')
 let args = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 console.log(args);
 
-function getRepoContributors(repoOwner, repoName, callback) {
-  return callback;
+function getRepoContributors(repoOwner, repoName, callback)
+{
+  let options =
+  {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers:
+    {
+      "Authorization": `token ${secrets.GITHUB_TOKEN}`,
+      "User-Agent": "request"
+    }
+  };
+
+  request(options.url, function(err, result, body)
+  {
+    callback(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
